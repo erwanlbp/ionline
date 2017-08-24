b98857b2872d1ac98f58134ec900e777ad1906a5
ErwanLBP <erwan.lbp@gmail.com>
2017-08-24 20:18:55 +0200

    Firebase setup
    - Init the connection
    - AddSerie()

--- main.go
+++ main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/erwanlbp/ionline/internal/handler"
 	"github.com/erwanlbp/ionline/internal/sys/config"
+	"github.com/erwanlbp/ionline/internal/sys/sysauth"
 )
 
 func main() {
+	var err error
+
 	fmt.Println("Start server", config.ServerHost())
 
+	// Init Firebase
+	err = sysauth.InitFirebase()
+	if err != nil {
+		fmt.Println("ERROR Initializing Firebase:", err.Error())
+		os.Exit(1)
+	}
+
 	router := handler.Init()
-	err := http.ListenAndServe(":"+config.ServerPort(), router)
+	err = http.ListenAndServe(":"+config.ServerPort(), router)
 	if err != nil {
 		fmt.Println("Server crashed :", err.Error())
 	}
